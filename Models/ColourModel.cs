53144908f91b9c0bc2e30c5e0d54f013b3131298
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-11-12 15:33:08 +0000

    feat: add User model and rename file

--- Models/ColourModel.cs
+++
@@ -1,7 +0,0 @@
-namespace j3.models;
-
-class Colour
-{
-    public int Id { get; set; }
-    public string? Hex { get; set; }
-}
---
+++ Models/Models.cs
@@ -0,0 +1,15 @@
+namespace j3.models;
+
+class Colour
+{
+    public int Id { get; set; }
+    public string? Hex { get; set; }
+}
+
+class User
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Hash { get; set; }
+}
