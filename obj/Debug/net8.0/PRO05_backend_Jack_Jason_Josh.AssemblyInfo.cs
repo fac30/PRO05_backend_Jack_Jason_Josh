af51045b1f7db6fb8664f19c7bd468e9250c9340
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-11-12 12:45:58 +0000

    build: add readme

--- obj/Debug/net8.0/PRO05_backend_Jack_Jason_Josh.AssemblyInfo.cs
+++ obj/Debug/net8.0/PRO05_backend_Jack_Jason_Josh.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRO05_backend_Jack_Jason_Josh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3a4f7b6504505a70767908f4dfe070961a549dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRO05_backend_Jack_Jason_Josh")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRO05_backend_Jack_Jason_Josh")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
