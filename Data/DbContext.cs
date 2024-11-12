bf807e63a542865c007acf60d4d69deaad2b763d
JoshCodedit <joshua.dixoncms@gmail.com>
2024-11-12 16:03:44 +0000

    feat: updated dbcontext configuration settings

---
+++ Data/DbContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace j3.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Collection> Collections { get; set; }
+        public DbSet<ColourCollection> ColourCollections { get; set; }
+        public DbSet<Colour> Colours { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Collection>().ToTable("collection");
+            modelBuilder.Entity<ColourCollection>().ToTable("colour_collection");
+            modelBuilder.Entity<Colour>().ToTable("colour");
+            modelBuilder.Entity<Comment>().ToTable("comment");
+            modelBuilder.Entity<User>().ToTable("user");
+
+            modelBuilder.Entity<ColourCollection>()
+                .HasKey(cc => new { cc.CollectionId, cc.ColourId });
+        }
+    }
+}
