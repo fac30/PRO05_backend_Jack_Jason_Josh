29100892da897c938fce762d6802d6e5dbc9bd5f
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-11-18 17:55:26 +0000

    refactor: remove most of user model

--- Models/Models.cs
+++ Models/Models.cs
@@ -31,13 +31,6 @@ public class Colour
 
 public class User : IdentityUser
 {
-    // public int Id { get; set; }
-
-    // public string Name { get; set; } = string.Empty;
-
-    // public string Email { get; set; } = string.Empty;
-    // public string Hash { get; set; } = string.Empty;
-
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
 }
