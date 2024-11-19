a52228d9e9f506ccc8b6b100777e195daa0a5fc7
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2024-11-19 11:23:04 +0000

    refactor: switch from block namespace to file namespace

--- Data/DbContext.cs
+++ Data/DbContext.cs
@@ -2,31 +2,28 @@ using J3.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace J3.Data
+namespace J3.Data;
+
+public class ColourContext : IdentityDbContext<User>, IColourContext
 {
-    public class ColourContext : IdentityDbContext<User>, IColourContext
+    public ColourContext(DbContextOptions<ColourContext> options)
+        : base(options) { }
+
+    public DbSet<Colour> Colours { get; set; }
+    public DbSet<Collection> Collections { get; set; }
+    public DbSet<ColourCollection> ColourCollections { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public ColourContext(DbContextOptions<ColourContext> options)
-            : base(options) { }
+        base.OnModelCreating(modelBuilder);
 
-        public DbSet<Colour> Colours { get; set; }
-        public DbSet<Collection> Collections { get; set; }
-        public DbSet<ColourCollection> ColourCollections { get; set; }
-        public DbSet<Comment> Comments { get; set; }
+        modelBuilder.Entity<Comment>().ToTable("comment");
+        modelBuilder.Entity<Collection>().ToTable("collection");
+        modelBuilder.Entity<ColourCollection>().ToTable("colour_collection");
+        modelBuilder.Entity<Colour>().ToTable("colour");
+        modelBuilder.Entity<User>().ToTable("user");
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Comment>().ToTable("comment");
-            modelBuilder.Entity<Collection>().ToTable("collection");
-            modelBuilder.Entity<ColourCollection>().ToTable("colour_collection");
-            modelBuilder.Entity<Colour>().ToTable("colour");
-            modelBuilder.Entity<User>().ToTable("user");
-
-            modelBuilder
-                .Entity<ColourCollection>()
-                .HasKey(cc => new { cc.CollectionId, cc.ColourId });
-        }
+        modelBuilder.Entity<ColourCollection>().HasKey(cc => new { cc.CollectionId, cc.ColourId });
     }
 }
